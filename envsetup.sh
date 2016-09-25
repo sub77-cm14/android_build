3c8d1b0d7e2ffcf4876f971fea88fb4f04b68d62
sub77 <sub77@ymail.com>
2016-09-25 01:49:33 +0000

    Revert "Fix JAVA_HOME"
    This reverts commit 138c5fec15af810849c3917de19d8eae63c8aef0.

--- envsetup.sh
+++ envsetup.sh
@@ -2509,7 +2509,7 @@ function set_java_home() {
                 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
                 ;;
             *)
-                export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+                export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
                 ;;
         esac
       fi
